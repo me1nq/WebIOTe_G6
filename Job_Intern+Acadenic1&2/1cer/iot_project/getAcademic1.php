94747d680382495ccccf7298f49d962261193a60
jsotsa <thanakit.jipromma@gmail.com>
2026-02-24 01:35:38 +0700

    yes

--- Job_Intern+Acadenic1&2/1cer/iot_project/getAcademic1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "university_db");
-
-$result = $conn->query("SELECT * FROM academic1 ORDER BY id DESC LIMIT 1");
-$row = $result->fetch_assoc();
-
-echo json_encode($row);
-?>
--- Job_Intern+Acadenic1&2/2cer/iote_project2/getAcademic2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "university_db");
-
-$result = $conn->query("SELECT * FROM academic2 ORDER BY id DESC LIMIT 1");
-
-$data = $result->fetch_assoc();
-
-echo json_encode($data);
-?>
